0d460d5d660de75feb2410a2e156da61378c253d
AnhPD <phiduyanh0605@gmail.com>
2025-04-01 15:07:39 +0700

    Printing logs

--- sentiment_analysis_app.py
+++ sentiment_analysis_app.py
@@ -1,6 +1,7 @@
 from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 from typing import Optional
 from datetime import datetime
+import os
 import streamlit as st
 
 class AppModel:
@@ -49,9 +50,9 @@ def main():
                 chat_message = st.chat_message('assistant')
                 chat_message.markdown(f'The sentence is {results['label'].capitalize()} with a probability of {results['score']:.2%}!')
                 
-                print(f'============================{datetime.now().strftime('%d-%m-%Y %H:%M:%S')}============================')
-                print(prompt_english)
-                print(results['label'], results['score'], end='\n\n')
+                os.write(f'============================{datetime.now().strftime('%d-%m-%Y %H:%M:%S')}============================')
+                os.write(prompt_english)
+                os.write(results['label'], results['score'], end='\n\n')
 
     if button2:
         with result_placeholder2.container():
@@ -66,9 +67,9 @@ def main():
                 else:
                     chat_message.markdown(f'Câu đã nhập mang sắc thái Trung lập với xác suất {results['score']:.2%}!')
 
-                print(f'============================{datetime.now().strftime('%d-%m-%Y %H:%M:%S')}============================')
-                print(prompt_vn)
-                print(results['label'], results['score'], end='\n\n')
+                os.write(f'============================{datetime.now().strftime('%d-%m-%Y %H:%M:%S')}============================')
+                os.write(prompt_vn)
+                os.write(results['label'], results['score'], end='\n\n')
 
 if __name__ == '__main__':
     main()
